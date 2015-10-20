9ceb5bead8d14aa0c913ea66b1ac5c4332dc8248
Robert Skwiat <robert@aadmins-MacBook-Pro.local>
2015-10-19 20:21:32 -0400

    initial commit of code

---
+++ imgur-app/src/components/header.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+
+module.exports = React.createClass({
+  render: function() {
+    return <nav className="navbar navbar-default header">
+    	<div className="container-fluid">
+    		<Link to="/" className="navbar-brand">Imgur Browser</Link>
+    		<ul className="nav navbar-nav navbar-right">
+    			<li><a href="/topic-1">Topic 1</a></li>
+    		</ul>
+    	</div>
+    </nav>
+  }
+});
